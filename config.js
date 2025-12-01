164a8a8197e2534adfcba47c6ce9783979db0d57
Web4 <167559384+Web4application@users.noreply.github.com>
2025-12-01 17:17:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(AIzaSyDMqMlf92Nhxd7-EzRsWRwtsycxbDzFpkA);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro",
+  model: "aura",
   tools: [
     {
       functionDeclarations: [
